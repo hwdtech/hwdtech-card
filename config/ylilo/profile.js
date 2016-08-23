46b1bd9791bd1e6e1c33bf17c81b8ccdf7ceba13
agalyamova <agalyamova@hwdtech.com>
2016-08-23 18:32:23 +0600

    Lilo CV

---
+++ config/ylilo/profile.js
@@ -0,0 +1,25 @@
+const _ = require('lodash');
+const moment = require('moment');
+
+const profile = require('./profile.json');
+const skills = require('./skills.json');
+const portfolio = require('require-all')({
+  dirname: __dirname + '/portfolio',
+  filter: /(.+)\.json$/,
+  recursive: false
+});
+
+const keywords =  `HWdTech, Portfolio, ${profile.name}, ${_.map(skills, 'name').join(', ')}`;
+
+module.exports = Object.assign({}, profile, {
+  keywords,
+  skills,
+  age: moment().diff(new Date(profile.birthday), 'year'),
+  employmentDate: moment(new Date(profile.employmentDate)).utc().format('DD MMMM YYYY'),
+  portfolio: _(portfolio).toArray().sortBy('index').value(),
+  interests: require('./interests.json'),
+  services: require('./services.json'),
+  social: require('./social-links.json'),
+  statistics: require('./statistics.json'),
+  workExperiences: require('./work-experiences.json'),
+});
